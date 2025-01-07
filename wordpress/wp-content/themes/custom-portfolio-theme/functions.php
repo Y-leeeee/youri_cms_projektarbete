4631f47c9bbb298e3c383c40abf840b9fd2872d6
Youri Lee <yourilee@Youris-Air.lan>
2025-01-07 22:34:05 +0100

    Update about.php

--- wordpress/wp-content/themes/custom-portfolio-theme/functions.php
+++ wordpress/wp-content/themes/custom-portfolio-theme/functions.php
@@ -49,3 +49,12 @@ function enqueue_font_awesome() {
 }
 add_action('wp_enqueue_scripts', 'enqueue_font_awesome');
 
+function register_menus() {
+    register_nav_menus(array(
+        'header-menu' => 'Header Menu',
+        'footer-menu' => 'Footer Menu',
+    ));
+}
+add_action('init', 'register_menus');
+
+
