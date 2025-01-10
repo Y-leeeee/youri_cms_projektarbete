6a88f9133cdb8639f611276650d0ea6da12363c9
Youri Lee <yourilee@Youris-Air.lan>
2025-01-10 10:56:53 +0100

    Add contact.php and update styles and functions

--- wordpress/wp-content/themes/custom-portfolio-theme/functions.php
+++ wordpress/wp-content/themes/custom-portfolio-theme/functions.php
@@ -49,6 +49,7 @@ function enqueue_font_awesome() {
 }
 add_action('wp_enqueue_scripts', 'enqueue_font_awesome');
 
+
 function register_menus() {
     register_nav_menus(array(
         'header-menu' => 'Header Menu',
