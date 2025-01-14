0b3272e598c1fcd13ca23170cf15fade9224e2c8
Youri Lee <yourilee@Youris-Air.lan>
2025-01-14 11:36:30 +0100

    Add profile photo in homepage

--- next-frontend/app/page.tsx
+++ next-frontend/app/page.tsx
@@ -1,18 +1,34 @@
 export default async function HomePage() {
-  const res = await fetch(
+  const pageRes = await fetch(
     "http://localhost:8888/cms_projektarbete/wordpress/wp-json/wp/v2/pages?slug=homepage"
   );
-  const data = await res.json();
+  const pageData = await pageRes.json();
 
-  if (!data.length || !data[0]?.content?.rendered) {
+  if (!pageData.length || !pageData[0]?.content?.rendered) {
     return <h1>No content available for the Homepage</h1>;
   }
 
-  const page = data[0];
+  const page = pageData[0];
+
+  let profilePhotoUrl = null;
+  if (page.acf.profile_photo) {
+    const mediaRes = await fetch(
+      `http://localhost:8888/cms_projektarbete/wordpress/wp-json/wp/v2/media/${page.acf.profile_photo}`
+    );
+    const mediaData = await mediaRes.json();
+    profilePhotoUrl = mediaData?.source_url;
+  }
 
   return (
     <div>
       <h1>{page.title.rendered}</h1>
+      {profilePhotoUrl && (
+        <img
+          src={profilePhotoUrl}
+          alt="Profile Photo"
+          style={{ maxWidth: "200px", borderRadius: "50%" }}
+        />
+      )}
       <div dangerouslySetInnerHTML={{ __html: page.content.rendered }} />
     </div>
   );
