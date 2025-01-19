c3dde4be23b3e7ca75c8cd86ccac0419733716cb
Youri Lee <yourilee@Youris-Air.lan>
2025-01-19 01:46:55 +0100

    Removed contact page and updated globals.css and MainMenu.tsx

--- next-frontend/app/components/MainMenu.tsx
+++ next-frontend/app/components/MainMenu.tsx
@@ -11,7 +11,7 @@ export default function MainMenu() {
     { name: "Home", path: "/" },
     { name: "Projects", path: "/projects" },
     { name: "About", path: "/about" },
-    { name: "Contact", path: "/contact" },
+
     { name: "Testimonials", path: "/testimonials" },
     { name: "Skills", path: "/skills" },
     { name: "Services", path: "/services" },
